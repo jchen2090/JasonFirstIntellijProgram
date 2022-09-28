25bc76bc432ae9e7239a13e7174dbfa734bc7bcb
unknown <BT_3W2_05@BKTECH.LOCAL>
2022-09-28 12:09:36 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        String name = "Jason";
+
+        System.out.println("Hello World!");
+        System.out.println("Hello " + name + "!");
+    }
+}
